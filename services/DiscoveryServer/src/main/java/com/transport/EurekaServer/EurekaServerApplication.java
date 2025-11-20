8f569a96d143ebc3422c41a97570ad268734f896
mdn753 <yassinemdnpro@gmail.com>
2025-11-20 21:21:04 +0100

    Added docker compose .

--- services/DiscoveryServer/src/main/java/com/transport/EurekaServer/EurekaServerApplication.java
+++ services/DiscoveryServer/src/main/java/com/transport/EurekaServer/EurekaServerApplication.java
@@ -1,8 +1,9 @@
 package com.transport.EurekaServer;
 
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
